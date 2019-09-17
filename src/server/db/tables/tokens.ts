9273e6a9d84bf5dc1ad824b47e0efa7ae86f5d7a
Kevin Auer <kvn35124@uab.edu>
2019-09-17 10:31:23 -0500

    problem with getting token info

--- src/server/db/tables/tokens.ts
+++ src/server/db/tables/tokens.ts
@@ -2,7 +2,7 @@ import {Query} from '../index';
 
 
 const insert = (userid: number) => Query(`insert into accesstokens (userid) value (?)`, [userid]);
-const update = (id: number, token: string) => Query(`update accesstokens set token ? where id = ?`, [token, id])
+const update = (id: number, token: string) => Query(`update accesstokens set token = ? where id = ?`, [token, id])
 
 export default {
     insert,
